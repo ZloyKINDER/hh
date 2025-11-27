acf3dc7ceeced782a8855121466c967d8d66dfdf
Timur Gorashchenko <timyr98239718@mail.ru>
2025-11-28 00:43:06 +0300

    add functionals

--- src/db/config.py
+++ src/db/config.py
@@ -7,5 +7,5 @@ def get_db_params() -> Dict[str, str]:
         "host": os.getenv("DB_HOST", "localhost"),
         "dbname": os.getenv("DB_NAME", "hh_vacancies"),
         "user": os.getenv("DB_USER", "postgres"),
-        "password": os.getenv("DB_PASSWORD", "Stalker9823!"),
+        "password": os.getenv("DB_PASSWORD", "your_pass"),
     }
